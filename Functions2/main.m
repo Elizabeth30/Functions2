4a7c0cedb55b81a88377b688e67b215fec9b8fcc
Elizabeth <ElizabethSanchez@crbc.net>
2015-04-22 10:07:58 -0400

    calcexp

--- Functions2/main.m
+++ Functions2/main.m
@@ -7,11 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
-
+int calcexp (int val, int exp){
+    int result=1;
+    int i=0;
+    for (i=0; i<exp; i++){
+        result= result*val;
+        
+    }
+    return result;
+    }
+float calcarea(int radius){
+  
+    int radiussquare=calcexp(radius,2);
+    return 3.14 * (float)radiussquare;
+    
+}
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        float i= calcarea(2);
+        NSLog(@"%f",i);
     }
     return 0;
 }
